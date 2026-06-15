c7b0a042412aea9a272b277422fd97eb7980c59a
MUHAMMED SHAKIR VA <111525692+shakirva@users.noreply.github.com>
2026-06-15 12:45:05 +0530

    fix: use correct event name for password reset

--- src/subscribers/auth-notifications.ts
+++ src/subscribers/auth-notifications.ts
@@ -72,5 +72,5 @@ export default async function authNotificationHandler({
 }
 
 export const config: SubscriberConfig = {
-  event: "auth.password_reset_request", 
+  event: "auth.password_reset", 
 };
