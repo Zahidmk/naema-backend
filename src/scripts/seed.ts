79fbf0c92946b6029f8694be8ad263525de89c6a
Zahid M K <91457816+Zahidmk@users.noreply.github.com>
2026-07-28 22:42:08 +0530

    Configure default store region for Kuwait and KWD

--- src/scripts/seed.ts
+++ src/scripts/seed.ts
@@ -28,7 +28,7 @@ export default async function seedDemoData({ container }: ExecArgs) {
   const salesChannelModuleService = container.resolve(Modules.SALES_CHANNEL);
   const storeModuleService = container.resolve(Modules.STORE);
 
-  const countries = ["gb", "de", "dk", "se", "fr", "es", "it"];
+  const countries = ["kw"];
 
   logger.info("Seeding store data...");
   const [store] = await storeModuleService.listStores();
@@ -58,11 +58,8 @@ export default async function seedDemoData({ container }: ExecArgs) {
       update: {
         supported_currencies: [
           {
-            currency_code: "eur",
+            currency_code: "kwd",
             is_default: true,
-          },
-          {
-            currency_code: "usd",
           },
         ],
         default_sales_channel_id: defaultSalesChannel[0].id,
@@ -74,8 +71,8 @@ export default async function seedDemoData({ container }: ExecArgs) {
     input: {
       regions: [
         {
-          name: "Europe",
-          currency_code: "eur",
+          name: "Kuwait",
+          currency_code: "kwd",
           countries,
           payment_providers: ["pp_system_default"],
         },
